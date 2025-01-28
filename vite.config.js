b542e87458fa8c9b6c01d0b9b5bd40b5d15007f9
Grégoire Prunet-Foch <gprunetf@MacBook-Air-de-Gregoire.local>
2025-01-28 12:45:07 -0400

    version final v2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // 🔥 Ajoute ceci pour éviter les erreurs de chargement des fichiers après déploiement
   plugins: [react()],
 })
