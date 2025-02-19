4b291ece3eeae89f36f629f730176c4eb73c963a
Grégoire Prunet-Foch <gprunetf@MacBook-Air-de-Gregoire.local>
2025-02-19 17:22:18 -0400

    fix size

--- src/components/Canvas/canvaContainers.jsx
+++ src/components/Canvas/canvaContainers.jsx
@@ -195,7 +195,7 @@ const CanvasContainer = ({ selectedBottle }) => {
   };
 
   return (
-    <div className="relative w-full h-full">
+    <div className="relative w-full h-screen">
       {!isModelLoaded && (
         <div className="absolute inset-0 flex items-center justify-center text-white">
           Chargement...
